8ec10fbf44507d02d5c22305c6b5d0127c68d594
berekettena <tena21bereket@gmail.com>
2024-03-14 14:43:58 +0300

    that takes a string str1 and a string str2 as arguments and returns a concatenated string requirement fixed

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,2 +1,13 @@
+#!/user/bin/env python3
+"""
+that takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """
+    str1: string 1
+    str2: string 2
+    """
     return (str1 + str2)
