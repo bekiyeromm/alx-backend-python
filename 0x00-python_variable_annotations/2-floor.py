57471fbfafbfa1ab85c84c5fa0cebc551b681938
berekettena <tena21bereket@gmail.com>
2024-03-14 14:52:54 +0300

    function floor which takes a float n as argument and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+takes a float n as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """return the floor of a float
+    n is input which is float type
+    """
+    return (int(n))
