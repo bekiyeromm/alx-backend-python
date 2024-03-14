153c9cb8c30a75fa19fe3152c33b3fe9769af3a2
berekettena <tena21bereket@gmail.com>
2024-03-14 16:21:09 +0300

    using valide annotating for list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,13 +3,18 @@
 function sum_list which takes a list input_list of floats as
 argument and returns their sum as a float.
 """
+from typing import List
 
 
 def sum_list(input_list: list[float]) -> float:
     """
     calculate the sum of float in a list
     """
+    """
+    an other way of finding sum
     sum: float = 0.0
     for list_item in input_list:
         sum += list_item
-    return (sum)
+    return flost(sum)
+    """
+    return float(sum(input_list))
