d4457468d3c160e25b81187474494832a13901d0
berekettena <tena21bereket@gmail.com>
2024-03-19 18:52:54 +0300

    return type modified

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ coroutin async_generator that takes no arguments
 """
 import asyncio
 import random
-from typing import List, AsyncGenerator
+from typing import List, Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     coroutin async_generator that takes no arguments
     which will loop 10 times, each time asynchronously wait 1 second
