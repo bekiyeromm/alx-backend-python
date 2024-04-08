45d5c976a863c243242e7e4fb01a83a8b3f64ed1
bekiyeromm <bk3tena@gmail.com>
2024-04-08 02:03:32 -0700

    parameterize a unit test and add assertRaises context manager to test that a KeyError is raised for the following inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,3 +26,26 @@ class TestAccessNestedMap(unittest.TestCase):
             expected:-expected test result
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'")
+    ])
+    def test_access_nested_map_exception(
+            self, nested_map, path, expected_message):
+        """
+        Use the assertRaises context manager to test that
+        a KeyError is raised
+        args:
+            nested_map:-nested map (input)
+            path:a sequence of key representing path to the value
+            expected_message: expected out put
+        """
+        with self.assertRaises(KeyError) as context:
+            data_item = nested_map
+            for key in path:
+                if isinstance(data_item, dict) and key in data_item:
+                    data_item = data_item[key]
+                else:
+                    raise KeyError(key)
+            self.assertEqual(str(context.exception), expected_message)
